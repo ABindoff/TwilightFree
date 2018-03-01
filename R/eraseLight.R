00495ced89cf6e5bb4dfd909ee497bf1f74243fa
Aidan Bindoff <Aidan.Bindoff@utas.edu.au>
2018-03-01 21:37:51 +1100

    add tool to 'erase' non-solar light

---
+++ R/eraseLight.R
@@ -0,0 +1,27 @@
+#' Manually erase non-solar light sources using mouse
+#'
+#' Click on the bottom-left and top-right corners of a box bounding the light you wish to erase
+#' This function is slow, please be patient.
+#' @param obs the data frame containing at minimum Date and Light data
+#' @param zlim the dynamic range of the plot
+#' @param offset an offset in hours used to centre night within the plot y-axis
+#' @export
+#' @importFrom BAStag lightImage
+#' @importFrom BAStag tsimageLocator
+#' @return data frame with edited Light data
+
+eraseLight <- function(obs, zlim = c(0, 64)){
+  ts <- lightImage(obs, offset = 18, zlim = zlim)
+  light.pol <- tsimageLocator(ts, n=2)
+  lp.index <- .bincode(light.pol, sort(obs$Date), right = T)
+  for(i in lp.index[1]:lp.index[2]){
+    if(strftime(obs$Date[i], "%H:%M:%S", tz = "GMT") >= strftime(light.pol[1], "%H:%M:%S", tz = "GMT") &
+       strftime(obs$Date[i], "%H:%M:%S", tz = "GMT") <= strftime(light.pol[2], "%H:%M:%S", tz = "GMT") &
+       obs$Date[i] >=light.pol[1] &
+       obs$Date[i] <= light.pol[2]){
+      obs$Light[i] <- 0
+    }
+  }
+  lightImage(obs, offset = 18, zlim = zlim)
+  return(obs)
+}

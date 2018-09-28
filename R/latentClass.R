e46cb82f8e83e3fac4351af15342a1af85e9c3ad
Aidan Bindoff <bindoffa@utas.edu.au>
2018-09-28 20:40:04 +1000

    require mclust

--- R/latentClass.R
+++ R/latentClass.R
@@ -2,6 +2,7 @@
 #'
 #' @param d data frame with Light, Date, delta columns at minimum
 #' @export
+#' @import mclust
 #' @importFrom magrittr %>%
 #' @return data frame with predicted day and night states in `state` column
 latentClass <- function(d, nstates = 3L, verbose = TRUE){
